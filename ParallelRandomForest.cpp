dd00f421da515a2da75c8e1297cb898d084bae89
Kirn Hans <kirn.hans@gmail.com>
2017-05-04 20:11:53 -0400

    Handles loan dataset too now

--- ParallelRandomForest.cpp
+++ ParallelRandomForest.cpp
@@ -18,9 +18,11 @@ void read_from_file(std:: string filename, int **subset, int M) {
   if (filename.find("cancer") != std::string::npos) {
 
     const int col_num = 10;
-    int len = 172;
+    int len = 513;
     std::vector<int> random_index;
     for (int i = 0; i < M; i++) {
+      subset[i] = new int[col_num];
+
       //read in random lines into our array
       int read_index;
       do {
@@ -38,27 +40,49 @@ void read_from_file(std:: string filename, int **subset, int M) {
 
 
       int *cur = subset[i];
-
       int line_count = in.get_file_line();
       while (line_count <= read_index) {
 	in.read_row(cur[0],cur[1],cur[2],cur[3],cur[4],cur[5],
 		    cur[6],cur[7],cur[8],cur[9]);
 	line_count++;
       }
-
-       printf("line number %d: ", read_index);
-      for (int j = 0; j < col_num; j++) {
-	printf("%d, ", subset[i][j]);
-      }
-      printf("\n");
-
-
-      printf("file loc %d\n", in.get_file_line());
     }
   }
 
+  else if (filename.find("loan") != std::string::npos) {
+
+    const int col_num = 17;
+    int len = 665159;
+    std::vector<int> random_index;
+    for (int i = 0; i < M; i++) {
+      subset[i] = new int[col_num];
+
+      //read in random lines into our array
+      int read_index;
+      do {
+	read_index = rand() % len;
+      }
+      while (contains(random_index, read_index));
+      random_index.push_back(read_index);
+
+      //set line
+
+      io::CSVReader<col_num> in(filename);
+      in.read_header(io::ignore_extra_column, "loan_amnt","funded_amnt","funded_amnt_inv","int_rate","installment","grade","sub_grade","annual_inc","dti","delinq_2yrs","inq_last_6mths","open_acc","pub_rec","revol_util","total_acc","initial_list_status","y");
+
+
+      int *cur = subset[i];
+      int line_count = in.get_file_line();
+      while (line_count <= read_index) {
+	in.read_row(cur[0],cur[1],cur[2],cur[3],cur[4],cur[5],
+		    cur[6],cur[7],cur[8],cur[9],cur[10],cur[11],
+		    cur[12],cur[13],cur[14],cur[15],cur[16]);
+	line_count++;
+      }
+    }
+  }
   else {
-    return; //FIX THIS SO THAT IT HANDLES THE OTHER DATASETS
+    return; //FIX THIS SO THAT IT HANDLES THE Marketing dataset
   }
 }
 
@@ -66,8 +90,6 @@ void read_from_file(std:: string filename, int **subset, int M) {
 void build_trees(std::string data_filename, forest *data_forest,
 		 int thread_count) {
   int **subset = new int*[4];
-  for (int i = 0; i < 4; i++)
-    subset[i] = new int[10];
   read_from_file("data/cancer/cancer_train.csv", subset, 4);
 }
 
